375d712ba550711a85acfbd53c1a84c401a79c56
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-04-04 20:21:06 +0200

    Polygon filtering works now as well

--- src/main/java/ch/uzh/ifi/hase/soprafs24/geo_admin_api/ResponseData.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/geo_admin_api/ResponseData.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class ResponseData {
 
-    private List<JsonNode> data = new ArrayList<JsonNode>();
+    public List<JsonNode> data = new ArrayList<JsonNode>();
 
     public ResponseData(List<JsonNode> data) {
         this.data = data;
@@ -43,17 +43,23 @@ public class ResponseData {
     // "status": "ueblich",
     // "label": "Punta Rosa"}
     // }
-    public List<JsonNode> filterByAttributes(String field, String value) {
-        return data.stream().filter(obj -> obj.get("attributes").get(field).asText().contains(value))
+    public void filterByAttributes(String field, String value) {
+        data = data.stream().filter(obj -> obj.get("attributes").get(field).asText().contains(value))
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
 
-    public List<JsonNode> filterByPolygon(List<JsonNode> json, double[][] polygon) {
-        return json.stream().filter(obj -> isPointInsidePolygon(
-                obj.get("poitns").get(0).get(0).asDouble(),
-                obj.get("poitns").get(0).get(1).asDouble(),
-                polygon))
-                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    public void filterByPolygon(List<JsonNode> json, double[][] polygon) {
+        data = json.stream().filter(
+            obj -> {
+                if (obj.get("geometry").get("rings") != null) {//filtering for polygons
+                    return false;
+                }
+                return isPointInsidePolygon(
+                    obj.get("geometry").get("points").get(0).get(0).asDouble(),
+                    obj.get("geometry").get("points").get(0).get(1).asDouble(),
+                    polygon);
+                }
+                ).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
 
     public List<JsonNode> selectRandomElements(int numElements) {
@@ -103,16 +109,22 @@ public class ResponseData {
         ResponseData data = FetchData.readLocalJson("gipfel.json");
         var data2 = FetchData.readLocalJson("huegel.json");
         data.addAll(data2);
-        data.filterByAttributes("sprachcode", "Hochdeutsch");
         double[][] polygon = new double[][] {
-                { 8.63201156032141, 47.55722864491296 },
-                { 8.641542724980663, 47.47757020920054 },
-                { 8.819943076188808, 47.46548121783862 },
-                { 8.877324542319437, 47.51708447356059 },
-                { 8.823688349653274, 47.563577608388435 },
-                { 8.63201156032141, 47.55722864491296 }
+            {688911, 260544},
+            {703344, 248102},
+            {721195, 258387},
+            {705567, 269337}
         };
-        // data.filterByPolygon(data.data, polygon); //doesn't work todo: fix
+        data.data.get(0).get("geometry").get("points").get(0).get(0).asDouble();
+        
+        var t = data.data.get(0);
+        var t2 = t.get("geometry");
+        var t3 = t2.get("points");
+        var t4 = t3.get(0);
+        var t5 = t4.get(0);
+        var t6 = t5.asDouble();
+        data.filterByPolygon(data.data, polygon);
+        data.filterByAttributes("sprachcode", "Hochdeutsch");
         var randomElements = data.selectRandomElements(10);
 
         // this is how the json is accessed
