15647a1cee9a1baea8c82095ce1edcb5c4645bc2
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-04-19 13:26:50 +0200

    fixed scheduling bug in gameEngine #37 #55

--- src/test/java/ch/uzh/ifi/hase/soprafs24/endpoint/controller/GameControllerUnitTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/endpoint/controller/GameControllerUnitTest.java
@@ -32,203 +32,207 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class GameControllerUnitTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @MockBean
-    private GameService gameService;
+        @MockBean
+        private GameService gameService;
 
-    @Test
-    public void getGameView() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        given(gameService.getGameView(Mockito.matches(gameId))).willReturn("{\"gameId\":\"1\"}");
-        var postRequest = get("/game/" + gameId + "/getView/").contentType(MediaType.APPLICATION_JSON);
+        @Test
+        public void getGameView() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                given(gameService.getGameView(Mockito.matches(gameId))).willReturn("{\"gameId\":\"1\"}");
+                var postRequest = get("/game/" + gameId + "/getView/").contentType(MediaType.APPLICATION_JSON);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.gameId", is("1")));
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.gameId", is("1")));
 
-        verify(gameService, Mockito.times(1)).getGameView(Mockito.matches(gameId));
-        verify(gameService, Mockito.times(0)).getGameView(Mockito.matches("somethingElse"));
-    }
+                verify(gameService, Mockito.times(1)).getGameView(Mockito.matches(gameId));
+                verify(gameService, Mockito.times(0)).getGameView(Mockito.matches("somethingElse"));
+        }
 
-    @Test
-    public void updateSettings() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        GameSettingsDTO settingsDTO = new GameSettingsDTO();
-        settingsDTO.setRounds(5);
-        settingsDTO.setGuessingTime(30);
-        settingsDTO.setId(1);
+        @Test
+        public void updateSettings() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                GameSettingsDTO settingsDTO = new GameSettingsDTO();
+                settingsDTO.setRounds(5);
+                settingsDTO.setGuessingTime(30);
+                settingsDTO.setId(1);
 
-        String jsonPayload = new ObjectMapper().writeValueAsString(settingsDTO);
+                String jsonPayload = new ObjectMapper().writeValueAsString(settingsDTO);
 
-        var postRequest = put("/game/" + gameId + "/updateSettings/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonPayload);
+                var postRequest = put("/game/" + gameId + "/updateSettings/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonPayload);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isNoContent());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isNoContent());
 
-        verify(gameService, Mockito.times(1)).updateSettings(Mockito.matches(gameId), Mockito.notNull(),
-                Mockito.notNull());
-        verify(gameService, Mockito.times(0)).updateSettings(Mockito.matches("somethingElse"), Mockito.notNull(),
-                Mockito.notNull());
-    }
+                verify(gameService, Mockito.times(1)).updateSettings(Mockito.matches(gameId), Mockito.notNull(),
+                                Mockito.notNull());
+                verify(gameService, Mockito.times(0)).updateSettings(Mockito.matches("somethingElse"),
+                                Mockito.notNull(),
+                                Mockito.notNull());
+        }
 
-    @Test
-    public void createGameTest() throws Exception {
-        // Arrange
-        CredentialsDTO credentialsDTO = new CredentialsDTO();
-        credentialsDTO.setId(1L);
-        credentialsDTO.setToken("1");
-        String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
-        var postRequest = post("/game/create/").contentType(MediaType.APPLICATION_JSON).content(jsonPayload);
+        @Test
+        public void createGameTest() throws Exception {
+                // Arrange
+                CredentialsDTO credentialsDTO = new CredentialsDTO();
+                credentialsDTO.setId(1L);
+                credentialsDTO.setToken("1");
+                String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
+                var postRequest = post("/game/create/").contentType(MediaType.APPLICATION_JSON).content(jsonPayload);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isCreated());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isCreated());
 
-        verify(gameService, Mockito.times(1)).createGame(Mockito.any());
-    }
+                verify(gameService, Mockito.times(1)).createGame(Mockito.any());
+        }
 
-    @Test
-    public void getAllGameIdsTest() throws Exception {
-        // Arrange
-        var postRequest = get("/game/").contentType(MediaType.APPLICATION_JSON);
+        @Test
+        public void getAllGameIdsTest() throws Exception {
+                // Arrange
+                var postRequest = get("/game/").contentType(MediaType.APPLICATION_JSON);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isOk());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isOk());
 
-        verify(gameService, Mockito.times(1)).getAllGameIds();
-    }
+                verify(gameService, Mockito.times(1)).getAllGameIds();
+        }
 
-    @Test
-    public void leaveGameTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        String playerId = "1";
-        var postRequest = put("/game/" + gameId + "/leave/")
-                .contentType(MediaType.APPLICATION_JSON).content("playerId=" + playerId);
+        @Test
+        public void leaveGameTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                String playerId = "1";
+                var postRequest = put("/game/" + gameId + "/leave/")
+                                .contentType(MediaType.APPLICATION_JSON).content("playerId=" + playerId);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isNoContent());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isNoContent());
 
-        verify(gameService, Mockito.times(1)).leaveGame(Mockito.matches(gameId), Mockito.matches(playerId));
-        verify(gameService, Mockito.times(0)).leaveGame(Mockito.matches("somethingElse"), Mockito.matches(playerId));
-    }
+                verify(gameService, Mockito.times(1)).leaveGame(Mockito.matches(gameId), Mockito.matches(playerId));
+                verify(gameService, Mockito.times(0)).leaveGame(Mockito.matches("somethingElse"),
+                                Mockito.matches(playerId));
+        }
 
-    @Test
-    public void joinGameTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        String displayName = "player1";
-        var postRequest = post("/game/" + gameId + "/join/")
-                .contentType(MediaType.APPLICATION_JSON).content("displayName=" + displayName);
+        @Test
+        public void joinGameTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                String displayName = "player1";
+                var postRequest = post("/game/" + gameId + "/join/")
+                                .contentType(MediaType.APPLICATION_JSON).content("displayName=" + displayName);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isFound());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isOk());
 
-        verify(gameService, Mockito.times(1)).joinGame(Mockito.matches(gameId), Mockito.matches(displayName));
-        verify(gameService, Mockito.times(0)).joinGame(Mockito.matches("somethingElse"), Mockito.matches(displayName));
-    }
+                verify(gameService, Mockito.times(1)).joinGame(Mockito.matches(gameId), Mockito.matches(displayName));
+                verify(gameService, Mockito.times(0)).joinGame(Mockito.matches("somethingElse"),
+                                Mockito.matches(displayName));
+        }
 
-    @Test
-    public void openLobbyTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        CredentialsDTO credentialsDTO = new CredentialsDTO();
-        credentialsDTO.setId(1L);
-        credentialsDTO.setToken("1");
-        String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
-        var postRequest = post("/game/" + gameId + "/openLobby/").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonPayload);
+        @Test
+        public void openLobbyTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                CredentialsDTO credentialsDTO = new CredentialsDTO();
+                credentialsDTO.setId(1L);
+                credentialsDTO.setToken("1");
+                String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
+                var postRequest = post("/game/" + gameId + "/openLobby/").contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonPayload);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isNoContent());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isNoContent());
 
-        verify(gameService, Mockito.times(1)).openLobby(Mockito.matches(gameId), Mockito.any());
-    }
+                verify(gameService, Mockito.times(1)).openLobby(Mockito.matches(gameId), Mockito.any());
+        }
 
-    @Test
-    public void startGameTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        CredentialsDTO credentialsDTO = new CredentialsDTO();
-        credentialsDTO.setId(1L);
-        credentialsDTO.setToken("1");
-        String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
-        var postRequest = post("/game/" + gameId + "/start/").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonPayload);
+        @Test
+        public void startGameTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                CredentialsDTO credentialsDTO = new CredentialsDTO();
+                credentialsDTO.setId(1L);
+                credentialsDTO.setToken("1");
+                String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
+                var postRequest = post("/game/" + gameId + "/start/").contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonPayload);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isNoContent());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isNoContent());
 
-        verify(gameService, Mockito.times(1)).startGame(Mockito.matches(gameId), Mockito.any());
-    }
+                verify(gameService, Mockito.times(1)).startGame(Mockito.matches(gameId), Mockito.any());
+        }
 
-    @Test
-    public void deleteGameTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        CredentialsDTO credentialsDTO = new CredentialsDTO();
-        credentialsDTO.setId(1L);
-        credentialsDTO.setToken("1");
-        String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
-        var postRequest = delete("/game/" + gameId + "/").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonPayload);
+        @Test
+        public void deleteGameTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                CredentialsDTO credentialsDTO = new CredentialsDTO();
+                credentialsDTO.setId(1L);
+                credentialsDTO.setToken("1");
+                String jsonPayload = new ObjectMapper().writeValueAsString(credentialsDTO);
+                var postRequest = delete("/game/" + gameId + "/").contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonPayload);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isNoContent());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isNoContent());
 
-        verify(gameService, Mockito.times(1)).deleteGame(Mockito.matches(gameId), Mockito.any());
-    }
+                verify(gameService, Mockito.times(1)).deleteGame(Mockito.matches(gameId), Mockito.any());
+        }
 
-    @Test
-    public void makeGuessTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        PostGuessDTO postGuessDTO = new PostGuessDTO();
-        postGuessDTO.setX(1.0);
-        postGuessDTO.setY(2.0);
-        String jsonPayload = new ObjectMapper().writeValueAsString(postGuessDTO);
-        var postRequest = post("/game/" + gameId + "/guess/").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonPayload);
+        @Test
+        public void makeGuessTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                PostGuessDTO postGuessDTO = new PostGuessDTO();
+                postGuessDTO.setX(1.0);
+                postGuessDTO.setY(2.0);
+                String jsonPayload = new ObjectMapper().writeValueAsString(postGuessDTO);
+                var postRequest = post("/game/" + gameId + "/guess/").contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonPayload);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isNoContent());
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isNoContent());
 
-        verify(gameService, Mockito.times(1)).submitAnswer(Mockito.matches(gameId), Mockito.any(), Mockito.any());
-    }
+                verify(gameService, Mockito.times(1)).submitAnswer(Mockito.matches(gameId), Mockito.any(),
+                                Mockito.any());
+        }
 
-    @Test
-    public void getSettingsTest() throws Exception {
-        // Arrange
-        String gameId = "ab3501ds";
-        var settings = new Settings();
-        settings.setRounds(4);
-        given(gameService.getSettingsView(Mockito.matches(gameId))).willReturn(settings);
-        var postRequest = get("/game/" + gameId + "/settings/").contentType(MediaType.APPLICATION_JSON);
+        @Test
+        public void getSettingsTest() throws Exception {
+                // Arrange
+                String gameId = "ab3501ds";
+                var settings = new Settings();
+                settings.setRounds(4);
+                given(gameService.getSettingsView(Mockito.matches(gameId))).willReturn(settings);
+                var postRequest = get("/game/" + gameId + "/settings/").contentType(MediaType.APPLICATION_JSON);
 
-        // Act
-        mockMvc.perform(postRequest)
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.rounds", is(4)));
+                // Act
+                mockMvc.perform(postRequest)
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.rounds", is(4)));
 
-        verify(gameService, Mockito.times(1)).getSettingsView(Mockito.matches(gameId));
-    }
+                verify(gameService, Mockito.times(1)).getSettingsView(Mockito.matches(gameId));
+        }
 
-    public static <T> org.hamcrest.Matcher<T> is(T value) {
-        return org.hamcrest.core.Is.is(value);
-    }
+        public static <T> org.hamcrest.Matcher<T> is(T value) {
+                return org.hamcrest.core.Is.is(value);
+        }
 }
