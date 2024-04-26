febbf404b62760ebf919d0ca6b8e7024815d43a8
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-04-26 12:18:46 +0200

    tried to guess formula for distance

--- src/main/java/ch/uzh/ifi/hase/soprafs24/game/entity/GeoLocation.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/game/entity/GeoLocation.java
@@ -23,8 +23,12 @@ public class GeoLocation {
     }
 
     public Double getDistanceTo(GeoLocation other) {
-        var lat_dif = x - other.x;
-        var lng_dif = y - other.y;
-        return Math.sqrt(Math.pow(lat_dif, 2) + Math.pow(lng_dif, 2));
+        // caluclating distance World Geodetic System 1984
+        double earthRadius = 6371008.8; // in meters
+        double latDistance = Math.toRadians(this.y - other.y);
+        double lonDistance = Math.toRadians(this.x - other.x);
+        return Math.sqrt(Math.pow(latDistance, 2)
+                + Math.pow(lonDistance, 2) * Math.cos(Math.toRadians(other.y)) * Math.cos(Math.toRadians(this.y)))
+                * earthRadius;
     }
 }
