5c020efec75af5411dc04ff4ced2a52407e3d40b
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-05-12 02:31:03 +0200

    Fixed GameStateDTO #124

--- src/main/java/ch/uzh/ifi/hase/soprafs24/endpoint/rest/dto/GameStateDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/endpoint/rest/dto/GameStateDTO.java
@@ -1,18 +1,18 @@
 package ch.uzh.ifi.hase.soprafs24.endpoint.rest.dto;
 
-import ch.uzh.ifi.hase.soprafs24.game.Enum.GameState;
+import ch.uzh.ifi.hase.soprafs24.game.Enum.RoundState;
 
 public class GameStateDTO {
-    private GameState gameState;
+    private RoundState roundState;
     private int currentRound;
     private long timeTillNextPhaseInMillis;
 
-    public GameState getGameState() {
-        return gameState;
+    public RoundState getRoundState() {
+        return roundState;
     }
 
-    public void setGameState(GameState gameState) {
-        this.gameState = gameState;
+    public void setRoundState(RoundState roundState) {
+        this.roundState = roundState;
     }
 
     public int getCurrentRound() {
