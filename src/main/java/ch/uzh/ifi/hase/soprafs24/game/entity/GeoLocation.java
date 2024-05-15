cbfe7ce82179801b84673cd93f017cacb1dff73d
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-05-15 18:03:31 +0200

    GeoLocation now handling null values

--- src/main/java/ch/uzh/ifi/hase/soprafs24/game/entity/GeoLocation.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/game/entity/GeoLocation.java
@@ -23,6 +23,9 @@ public class GeoLocation {
     }
 
     public Double getDistanceTo(GeoLocation other) {
+        if (this.isNull() || other.isNull()) {
+            return null;
+        }
         // caluclating distance World Geodetic System 1984
         double earthRadius = 6371008.8; // in meters
         double latDistance = Math.toRadians(this.y - other.y);
@@ -30,5 +33,9 @@ public class GeoLocation {
         return Math.sqrt(Math.pow(latDistance, 2)
                 + Math.pow(lonDistance, 2) * Math.cos(Math.toRadians(other.y)) * Math.cos(Math.toRadians(this.y)))
                 * earthRadius;
+    }
+    
+    public boolean isNull() {
+        return (y == null || x == null);
     }
 }
