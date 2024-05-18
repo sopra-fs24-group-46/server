0c84819c843f26e66fa0482f16dc37c2d92769cd
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-05-18 17:33:23 +0200

    Updated SettingsView to contain all settings for lobby. #6

--- src/main/java/ch/uzh/ifi/hase/soprafs24/game/View/SettingView.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/game/View/SettingView.java
@@ -1,7 +1,10 @@
 //This is an immutable wrapper for the Settings Class
 package ch.uzh.ifi.hase.soprafs24.game.View;
 
-import ch.uzh.ifi.hase.soprafs24.user.User;
+import java.util.List;
+
+import ch.uzh.ifi.hase.soprafs24.game.entity.LocationTypes;
+import ch.uzh.ifi.hase.soprafs24.geo_admin_api.RegionType;
 
 public interface SettingView {
     public Long getHostUserId();
@@ -17,4 +20,13 @@ public interface SettingView {
     public Integer getMapRevealTime();
 
     public Integer getLeaderBoardTime();
+    
+    public String getRegion();
+
+    public RegionType getRegionType();
+
+    public List<LocationTypes> getLocationTypes();
+
+    public List<String> getLocationNames();
 }
+
